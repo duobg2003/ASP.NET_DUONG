8aa860568990c0402baf9e031e5bec0cfd3fe3c1
duobg2003 <nqd222003@gmail.com>
2025-03-29 09:03:45 +0700

    Initial commit

---
+++ Model/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace nguyenquocduong_2122110443.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public string Image
+        {
+            get; set;
+        }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace nguyenquocduong_2122110443
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
